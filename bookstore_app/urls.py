2cf4939750cb57195368acf784c961e03676da19
Przemek Tazbirek <przemek10197@gmail.com>
2020-05-31 21:35:20 +0200

    set books' CRUD and user authentication

---
+++ bookstore_app/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('', include('rest_auth.urls')),
+    path('registration/', include('rest_auth.registration.urls')),
+]
